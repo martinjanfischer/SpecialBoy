ca6054915ff3d8adaeac1f1e2b92fa9226c55a76
Martin Fischer <martin.jan.fischer@arcor.de>
2024-09-16 18:44:08 +0200

    einsammeln totalscore https://www.youtube.com/watch?v=8KJLqgqHE3c

---
+++ Assets/Scripts/einsammeln.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class einsammeln : MonoBehaviour
+{
+    totalScore totalScoreScipt;
+    void start()
+    {
+        totalScoreScipt = GameObject.Find("totalScore").GetComponent<totalScore>();
+    }
+
+    //update is called once per frame
+    void update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/totalScore.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class totalScore : MonoBehaviour
+{
+    public int totalscore;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
